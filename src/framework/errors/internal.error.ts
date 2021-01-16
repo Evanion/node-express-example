c15a7d96148b2ed19d3aab93ca6e9cdbe714f540
Mikael Pettersson <evanion@icloud.com>
2021-01-16 03:03:46 +0100

    cleanup

--- src/framework/errors/internal.error.ts
+++
@@ -1,3 +0,0 @@
-import { BaseException } from "./base.error";
-
-export class InternalException extends BaseException {}
--- src/framework/index.ts
+++ src/framework/index.ts
@@ -1,7 +1,2 @@
-import "reflect-metadata";
-
-export * from "./constants";
 export * from "./controller";
 export * from "./middleware";
-export * from "./decorators";
-export * from "./enums";
